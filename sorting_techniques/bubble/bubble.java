66a10cc9ea8fc780206612b2adc3e92a43c2366e
Shahzaneer Ahmed ⚡ <shahzaneer.dev@gmail.com>
2024-07-23 23:45:06 +0500

    recursive bubble + insertion : ✅

--- sorting_techniques/bubble/bubble.java
+++ sorting_techniques/bubble/bubble.java
@@ -6,8 +6,13 @@ public static void main(String[] args) {
 //         int [] sortedArray = bubbleSort(array);
 //         System.out.println(Arrays.toString(sortedArray));
 
+// int [] array = {13,17,15,6,7,9};
+//         int [] sortedArray = bubbleSortOpmtimized(array);
+//         System.out.println(Arrays.toString(sortedArray));
+
+
 int [] array = {13,17,15,6,7,9};
-        int [] sortedArray = bubbleSortOpmtimized(array);
+        int [] sortedArray = bubbleSortRecursive(array, array.length);
         System.out.println(Arrays.toString(sortedArray));
 }   
 
@@ -26,6 +31,22 @@ static int [] bubbleSort(int [] array){
     return array;
 }
 
+static int [] bubbleSortRecursive(int [] array, int size){
+    if(size == 1) return array;
+    int didSwap = 0;
+    for(int j = 0; j<= size - 2; j++){
+        if(array[j] > array[j+1]){
+            int temp = array[j];
+            array[j] = array[j+1];
+            array[j+1] = temp;
+            didSwap = 1;
+        }
+      
+    }
+    if (didSwap == 0) return array;
+    bubbleSortRecursive(array, --size);
+    return array;
+}
 static int [] bubbleSortOpmtimized(int [] array){
     for(int i = array.length - 1; i>=1; i--){
         int didSwap = 0;
