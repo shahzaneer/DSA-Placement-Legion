7dd53c6afa64a2c99d1ae00b4cdee018c6d428dc
Shahzaneer Ahmed ⚡ <shahzaneer.dev@gmail.com>
2024-07-23 23:45:53 +0500

    recursive bubble+insertion added

--- sorting_techniques/insertion/insertion.java
+++ sorting_techniques/insertion/insertion.java
@@ -2,8 +2,11 @@ import java.util.Arrays;
 
 public class insertion {
 public static void main(String[] args) {
+    // int [] array = {13,17,15,6,7,9};
+    //     int [] sortedArray = insertionSort(array);
+    //     System.out.println(Arrays.toString(sortedArray));
     int [] array = {13,17,15,6,7,9};
-        int [] sortedArray = insertionSort(array);
+        int [] sortedArray = insertionSortRecursive(array, 0);
         System.out.println(Arrays.toString(sortedArray));
 }   
  static int [] insertionSort(int [] array){
@@ -18,4 +21,18 @@ public static void main(String[] args) {
     }
     return array;
  }
+
+ static int [] insertionSortRecursive(int [] array, int n){
+   if (n == array.length) return array;
+   int j = n;
+   while(j > 0 && array[j-1] > array[j]){
+    int temp = array[j-1];
+            array[j-1] = array[j];
+            array[j] = temp;
+            j--;
+   }
+   insertionSortRecursive(array, n+1);
+   return array;
+ }
+
 }
