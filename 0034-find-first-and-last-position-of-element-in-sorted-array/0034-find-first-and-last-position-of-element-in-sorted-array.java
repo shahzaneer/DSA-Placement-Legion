f14576a696a0b6909dd4b78077217af4a0c83e13
Shahzaneer Ahmed ⚡ <SP21-BCS-087@ISBSTUDENT.COMSATS.EDU.PK>
2024-08-22 10:33:21 +0500

    Time: 0 ms (100%), Space: 45.9 MB (30.8%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,48 +1,52 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
         int firstOccur = firstOccurrenceOptimal(nums, target);
-        if(firstOccur == -1) return new int [] {-1,-1};
+        if (firstOccur == -1)
+            return new int[] { -1, -1 };
         int lastOccur = lastOccurrenceOptimal(nums, target);
-        return new int [] {firstOccur, lastOccur};
+        return new int[] { firstOccur, lastOccur };
     }
 
+    static int firstOccurrenceOptimal(int[] array, int key) {
+        int low = 0;
+        int high = array.length - 1;
+        // int lowerBound = array.length;
+        int firstOccurent = -1;
 
-    static int firstOccurrenceOptimal(int [] array, int key){
-    int low = 0;
-    int high = array.length - 1;
-    // int lowerBound = array.length;
-    int firstOccurent = -1;
+        while (low <= high) {
+            int mid = (low + high) / 2;
 
-    while(low <= high){
-        int mid = (low + high) / 2;
-
-        // Case 1: we found element or we found element > key (a close answer maybe) 
-        if(array[mid] >= key){
-            if(array[mid] == key) firstOccurent = mid;
-            // lowerBound = mid;
-            high = mid - 1;
-        }
-        // case 2: we were not able to find so reducing searchSpace
-        else low = mid + 1;
-    }
-    
-    return firstOccurent;
-    }
-
-
-    static int lastOccurrenceOptimal(int [] array, int key){
-        int upperBound = array.length;
-        int low = 0, high = array.length-1;
-        while(low <=high){
-            int mid = (low + high)/2;
-
-            if(array[mid] > key){
-                upperBound = mid;
+            // Case 1: we found element or we found element > key (a close answer maybe)
+            if (array[mid] >= key) {
+                if (array[mid] == key)
+                    firstOccurent = mid;
+                // lowerBound = mid;
                 high = mid - 1;
             }
-            else low = mid + 1;
+            // case 2: we were not able to find so reducing searchSpace
+            else
+                low = mid + 1;
+        }
+
+        return firstOccurent;
+    }
+
+    static int lastOccurrenceOptimal(int[] array, int key) {
+        int upperBound = array.length;
+        int low = 0, high = array.length - 1;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+
+            if (array[mid] > key) {
+                upperBound = mid;
+                high = mid - 1;
+            } else
+                low = mid + 1;
         }
         int lastOccurrence = upperBound - 1;
+        if (array[lastOccurrence] != key || lastOccurrence == array.length)
+            return -1;
+
         return lastOccurrence;
     }
 }
