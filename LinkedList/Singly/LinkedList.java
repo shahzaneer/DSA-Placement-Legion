756f9e4e5e328cd8fe7b3d13b532a3c2f2ec5bb5
Shahzaneer Ahmed ⚡ <shahzaneer.dev@gmail.com>
2024-09-19 21:13:51 +0500

    singlyLL insert + delete

--- LinkedList/Singly/LinkedList.java
+++ LinkedList/Singly/LinkedList.java
@@ -1,13 +1,16 @@
 public class LinkedList {
     public static void main(String[] args) {
         Node head = Node.makeSinglyLinkedList(new int []{1,2,3,4,5,6,7});
-        Node mover = head;
-        while(mover!= null){
-            System.out.println(mover.data);
-            mover = mover.next;  
-        }
-
+    
+        Node.LinkedListTraversal(head);
         System.out.println(Node.lengthOfLL(head));
         System.out.println(Node.searchInLL(head, 7));
+
+        // head = Node.insertNode(head, 0, 8);
+        // head = Node.insertNode(head, 4, 12);
+        // head = Node.insertNode(head,7,99);
+        head = Node.insertNode(head,17,99);
+
+        Node.LinkedListTraversal(head);
     }
 }
--- LinkedList/Singly/Node.java
+++ LinkedList/Singly/Node.java
@@ -28,6 +28,83 @@ public class Node {
         return head;
     }
 
+    static void LinkedListTraversal(Node head){
+        Node mover = head;
+        while(mover!= null){
+            System.out.println(mover.data);
+            mover = mover.next;  
+        }
+
+    }
+    static Node insertNode(Node head,int position, int data){
+        Node newNode = new Node(data);
+        if(position == 0){
+            newNode.next = head;
+            head = newNode;
+            return head;
+        }
+        else{
+            Node current = head;
+            Node previous = null;
+            int counter = 1;
+            while(counter!=position && current.next!=null){
+                counter++;
+                previous = current;
+                current = current.next;
+            }
+            if(current.next == null){
+                current.next = newNode;
+                return head;
+            }
+            previous.next = newNode;
+            newNode.next = current;
+            return head;
+        }
+
+    }
+    // LC - 69
+    static void delete(Node node){
+        // Copy the value of the next node to the current node
+        node.data = node.next.data;
+        
+        // Point the current node to the next of the next node (bypass the next node)
+        node.next = node.next.next;
+    }
+    static Node deleteNode(Node head, int data){
+        if(head.data == data){
+            Node temp = head;
+            head = head.next;
+            temp = null;
+            return head;
+        }
+        Node mover = head;
+        Node previous = null;
+        while(mover!=null || mover.data != data){
+            previous = mover;
+            mover = mover.next;
+        }
+
+        Node temp = mover;
+        if(mover.next == null){
+            previous.next = null;
+            mover = null;
+            return head;
+        }
+        previous.next = mover.next;
+        mover = null;
+        return head;
+    }
+    // LC - 70 
+    static Node middleNode(Node head){
+        Node slowPointer = head;
+        Node fastPointer = head;
+        while(fastPointer!=null && fastPointer.next!=null){
+            slowPointer = slowPointer.next;
+            fastPointer = fastPointer.next.next;
+        }
+        return slowPointer;
+    }
+
     static int lengthOfLL(Node head){
         int counter = 0;
         Node current = head;
