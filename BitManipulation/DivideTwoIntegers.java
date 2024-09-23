9e322141f9a9114a0e56c4d6e92c8e224e5c1c07
Shahzaneer Ahmed ⚡ <shahzaneer.dev@gmail.com>
2024-09-23 15:51:27 +0500

    bit manipulation basics 2

---
+++ BitManipulation/DivideTwoIntegers.java
@@ -0,0 +1,22 @@
+public class DivideTwoIntegers {
+    public static void main(String[] args) {
+     System.out.println(divideTwoIntegersBrute(10, 2));
+        
+    }
+
+    //! Brute
+    static int divideTwoIntegersBrute(int dividend, int divisor){
+        boolean isNegative = false;
+        if((dividend >0 && divisor < 0) || (dividend < 0 && divisor > 0)) isNegative = true;
+        int counter = 0, sum = 0;
+        long dividendAbs = Math.abs(dividend), divisorAbs = Math.abs(divisor);
+        while(sum + divisorAbs <= dividendAbs){
+                counter += 1;
+                sum += divisorAbs;
+            }
+
+            return isNegative? -counter: counter;
+        }
+
+    }
+
--- BitManipulation/Ithbit.java
+++ BitManipulation/Ithbit.java
@@ -16,6 +16,8 @@ public class Ithbit {
         // Toggle the i-th bit
         int toggle = ToggleBit(unset, i);
         System.out.println("After toggling bit " + i + ": " + toggle); // 7 (0111)
+
+    //! for one liners we only need to make ready the mask (the second number from which we can perform n binaryOperator mask to gain answer)
     }
 
     //! Setbit the ith if its 1 stays 1   otherwise make it 1
@@ -32,5 +34,82 @@ public class Ithbit {
         return (num ^ (1<<i));
     }
 
+    // ! unset the rightmost first bit 
+    static int unsetRightmostBit(int num){
+        return (num & num - 1);
+    }
+    // ! set the rightmost first bit 
+    static int setRightMostUnsetBit(int num){
+        return (num | (num + 1));
+    }
+    
+    // ! Toggle the rightmost first bit 
+    static int toggleRightmostBit(int num){
+        return (num ^ (num - 1));
+    }
+
+    // ! Check if a number is odd or not
+    static boolean isOdd(int num){
+        return ((num & 1) == 1);
+    }
+
+    // ! Divide by two without using / or modulo operator
+    static int divisionByTwo(int num){
+        return (num >> 1);
+    }
+
+    // ! check if the number is a power of two or not.
+    static boolean isPowerOfTwo(int num){
+        // we know that for being power of two we only have 1 set bit in the binary.
+        // that set bit the rightmost bit.
+        // if we unset it it will become zero
+        // so
+        return ((num & num-1) == 0);
+    }
+
+
+    // ! Brute: Count the number of setBits
+    static int countSetBits(int num){
+        // there is no single liner
+        int n = num;
+        int counter = 0;
+        while(n!=1){
+            if(n%2 == 1) counter+=1;
+            n = n/2;
+        }
+        if(n == 1) counter+=1;
+        return counter;
+    }
+
+    // ! Bit-Operations: Count the number of setBits
+    static int countSetBitsBitWise(int num){
+        // there is no single liner
+        int n = num;
+        int counter = 0;
+        while(n!=1){
+            // if(n%2 == 1) counter+=1; //Odd check can be written as
+            // if(n&1 == 1) counter+=1; //more precisely
+            counter+= n&1;
+
+            // n = n/2; the division by two is rightshit by 1  as in binary search you do (low+high) >> 1;
+            n =  n >> 1;
+
+        }
+        if(n == 1) counter+=1;
+        return counter;
+    }
+
+    // ! Optimal: CountTheSetBits
+    static int countSetBitsBitWiseOptimal(int num){
+        // there is no single liner
+        int n = num;
+        int counter = 0;
+        while(n!=0){
+            // one by one removing the rightmost setBit at the end we will have zero.
+            n = n & (n-1);
+            counter+=1;
+        }
+        return counter;
+    }
 }
 
