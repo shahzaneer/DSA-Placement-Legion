c0d0bd75d6f77fa224f4ffe04d27034824c30b5f
Shahzaneer Ahmed ⚡ <shahzaneer.dev@gmail.com>
2024-10-24 18:16:33 +0500

    Time: 92 ms (33.32%), Space: 42.6 MB (64.84%) - LeetHub

---
+++ 1890-sum-of-beauty-of-all-substrings/1890-sum-of-beauty-of-all-substrings.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int beautySum(String s) {
+       // ! GetALLSubStrings
+        int sumOfAllBeauties = 0; 
+        for(int i=0; i<s.length(); i++){
+            int [] frequency = new int [26];
+            for(int j = i; j<s.length(); j++){
+                frequency[s.charAt(j) - 'a']++;   // 'a' - 'a' == 0 and so on
+                int maxFrequency = getMax(frequency);
+                int minFrequency = getMin(frequency);
+                int beauty = maxFrequency - minFrequency;
+                sumOfAllBeauties+=beauty;
+            }
+        }
+
+        return sumOfAllBeauties; 
+    }
+
+    private static int getMax(int[] frequency) {
+        int max = Integer.MIN_VALUE;
+        for(int i=0; i<frequency.length; i++){
+            if(frequency[i]!=0) max = Math.max(max, frequency[i]);
+        }
+        return max;
+    }
+
+    private static int getMin(int[] frequency) {
+        int min = Integer.MAX_VALUE;
+        for(int i=0; i<frequency.length; i++){
+            if(frequency[i]!=0) min = Math.min(min, frequency[i]);
+        }
+        return min;
+    }
+}
