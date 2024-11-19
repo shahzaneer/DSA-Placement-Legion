b515dcae2fc30a5fac305dae2a8c18b0c56f9d81
Shahzaneer Ahmed ⚡ <shahzaneer.dev@gmail.com>
2024-11-19 21:41:22 +0500

    Time: 5 ms (87.05%), Space: 45.2 MB (56.7%) - LeetHub

---
+++ 0735-asteroid-collision/0735-asteroid-collision.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int[] asteroidCollision(int[] asteroids) {
+        // monotonic stack
+        List<Integer> st = new LinkedList<>();
+
+        for(int i=0; i<asteroids.length; i++){
+
+            if(asteroids[i] > 0) st.addLast(asteroids[i]);
+
+            else{
+                while(!st.isEmpty() && st.getLast() > 0 && st.getLast() < Math.abs(asteroids[i])){
+                    st.removeLast();
+                }
+
+                if(!st.isEmpty() && Math.abs(asteroids[i]) == st.getLast()){
+                    st.removeLast();
+            } 
+            else if(!st.isEmpty() && st.getLast() < 0){
+                st.addLast(asteroids[i]);
+            }
+            else if(st.isEmpty()){
+                st.addLast(asteroids[i]);
+            }
+            }
+        
+    }
+
+    int [] answer = new int [st.size()];
+
+   for(int i=0; i<st.size(); i++){
+    answer[i] = st.get(i);
+   }
+    return answer;
+    }
+}
