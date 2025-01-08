04913649b7f4d5a6bb951331cb1c1d1681c68dac
Shahzaneer Ahmed ⚡ <shahzaneer.dev@gmail.com>
2025-01-08 23:44:18 +0500

    isSubsequence

---
+++ Strings/isSubsequence.java
@@ -0,0 +1,21 @@
+public class isSubsequence{
+    public static void main(String[] args) {
+        
+    }
+
+    static public boolean isSubsequence(String s, String t) {
+        if(s.isEmpty()) return true;
+        if(s.length() > t.length()) return false;
+
+        int sIndex = 0, tIndex = 0;
+        while(tIndex!= t.length()){
+            if(sIndex == s.length() - 1 && t.charAt(tIndex) == s.charAt(sIndex)) return true;
+            if(t.charAt(tIndex) == s.charAt(sIndex)){
+                sIndex++;
+            }
+            tIndex++;
+        }
+
+        return false;
+    }
+}
